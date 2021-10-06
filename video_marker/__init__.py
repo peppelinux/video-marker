32fbae718d0a66947f20593a83661eec8b14a171
peppelinux <giuseppe.demarco@unical.it>
2021-10-06 14:58:08 +0200

    v0.6.0
    - feat: `--max-recording-exceptions`
    - feat: `--exception-grace-period`
    - feat: new I/O methods: VideoMarker.release_device(), VideoMarker._load_video_source(), VideoMarker._assert_frame()

--- video_marker/__init__.py
+++ video_marker/__init__.py
@@ -3,5 +3,5 @@ import signal
 import sys
 import logging
 
-__version__ = "0.5.3"
+__version__ = "0.6.0"
 logger = logging.getLogger(__name__)
--- video_marker/video_marker.py
+++ video_marker/video_marker.py
@@ -3,6 +3,7 @@ import logging
 import numpy as np
 import os
 import signal
+import time
 
 from .exceptions import MissingFrame
 from . utils import CFEVideoConf, image_resize
@@ -25,10 +26,11 @@ class VideoMarker(object):
                  watermark_size = 50,
                  watermark_inverted = False,
                  slow_down_by = 0,
+                 exception_grace_period = 0.2,
+                 max_recording_exceptions = 4,
                  monitor = False, **kwargs
         ):
-        video_source = int(video_cap) if video_cap.isdigit() else video_cap
-        self.capture = cv2.VideoCapture(video_source)
+        self.video_source = int(video_cap) if video_cap.isdigit() else video_cap
         self.video_size = CFEVideoConf.STD_DIMENSIONS[resolution]
 
         self.slow_down_by = slow_down_by
@@ -40,13 +42,51 @@ class VideoMarker(object):
         self.post_media = post_media
         self.monitor = monitor
 
-        self.vconf = dict(
+        self.save_path = save_path
+        self.frames_per_seconds = frames_per_seconds
+
+        self.video_writer_config_params = dict(
             res = resolution,
             filepath = save_path,
         )
-        config = CFEVideoConf(self.capture, **self.vconf)
-        self.video_writer = cv2.VideoWriter(save_path, config.video_type, frames_per_seconds, config.dims)
+
         self.state = 0
+
+        self.exception_grace_period = exception_grace_period
+        self.max_recording_exceptions = max_recording_exceptions
+        self.exceptions_counter = 0
+
+        self._load_video_source()
+
+    def release_device(self):
+        self.capture.release()
+        self.video_writer.release()
+
+    def _load_video_source(self):
+        """
+        Load and reload a video source runtime
+        """
+        if hasattr(self, 'capture'):
+            del(self.capture)
+        if hasattr(self, 'video_writer_config'):
+            del(self.video_writer_config)
+            del(self.video_writer)
+
+        self.capture = cv2.VideoCapture(self.video_source)
+        self.video_writer_config = CFEVideoConf(
+            self.capture, **self.video_writer_config_params
+        )
+        self.video_writer = cv2.VideoWriter(
+            self.save_path, self.video_writer_config.video_type,
+            self.frames_per_seconds, self.video_writer_config.dims
+        )
+
+    @staticmethod
+    def _assert_frame(frame):
+        if not hasattr(frame, 'any') or not frame.any():
+            return False
+        else:
+            return True
 
     def get_watermark(self):
         if not self.watermark_fpath:
@@ -89,6 +129,14 @@ class VideoMarker(object):
                 break
         intro.release()
 
+    def _assert_videodevice(self):
+        # frame sample
+        ret, frame = self.capture.read()
+        if not self._assert_frame(frame):
+            logger.critical("Missing frame in .capture.read() ... exit")
+            raise MissingFrame("OpenCV frame is None, check video device")
+
+
     def record_video(self, watermark = None):
         if isinstance(watermark, np.ndarray):
             watermark_h, watermark_w, watermark_c = watermark.shape
@@ -109,11 +157,7 @@ class VideoMarker(object):
         COLOR_BGRA2BGR = cv2.COLOR_BGRA2BGR
         COLOR_BGR2BGRA = cv2.COLOR_BGR2BGRA
 
-        # frame sample
         ret, frame = self.capture.read()
-        if not hasattr(frame, 'any') or not frame.any():
-            logger.critical("Missing frame in .capture.read() ... exit")
-            raise MissingFrame("OpenCV frame is None, check video device")
         frame = cvtColor(frame, cv2.COLOR_BGR2BGRA)
 
         frame_h, frame_w, frame_c = frame.shape
@@ -129,8 +173,27 @@ class VideoMarker(object):
                     overlay[h_offset + i, w_offset+ j] = watermark[i,j]
 
         while (self.state >= 0):
+            if self.exceptions_counter >=self.max_recording_exceptions:
+                excp_msg = (
+                    "Exception during video recording with "
+                    f"exceptions_counter={self.exceptions_counter} "
+                    f"and exception_grace_period={self.exception_grace_period}"
+                )
+                logger.critical(excp_msg)
+                raise MissingFrame(
+                    excp_msg
+                )
+
             # Capture frame-by-frame
             ret, frame = self.capture.read()
+            if not self._assert_frame(frame):
+                self.exceptions_counter += 1
+                time.sleep(self.exception_grace_period)
+                self._load_video_source()
+                continue
+            else:
+                self.exceptions_counter = 0
+
             frame = cvtColor(frame, COLOR_BGR2BGRA)
 
             if _add_watermark:
@@ -161,12 +224,12 @@ class VideoMarker(object):
         self.state = 1
         signal.signal(signal.SIGTERM, self.signal_handler)
         logger.info(f"Starting video recording with {self.repr_params()}")
+        self._assert_videodevice()
         self.pre_post_video(media = self.pre_media)
         _watermark = self.get_watermark()
         self.record_video(watermark = _watermark)
         self.pre_post_video(media = self.post_media)
 
         # When everything done, release the capture
-        self.capture.release()
-        self.video_writer.release()
+        self.release_device()
         cv2.destroyAllWindows()
